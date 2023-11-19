201c8ebee30304f7d88ede14e680678f829162b5
Eddy Gervinet <eddygervinet@gmail.com>
2023-11-19 15:13:26 +0100

    Animate js introduction

--- assets/app.js
+++ assets/app.js
@@ -9,6 +9,7 @@ import './bootstrap.js';
 // any CSS you import will output into a single css file (app.css in this case)
 import 'normalize.css';
 import 'bulma/css/bulma.css';
+import 'animate.css';
 
 import './styles/prism.css';
 import './styles/cheatsheet.css';
