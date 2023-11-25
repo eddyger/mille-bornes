9489e142c13e5a5b433dd368bc0ab190c04b9364
Eddy Gervinet <eddygervinet@gmail.com>
2023-11-25 11:39:30 +0100

    Send email after successfull registration

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -5,16 +5,19 @@ namespace App\Controller;
 use App\Entity\User;
 use App\Form\RegistrationFormType;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class RegistrationController extends AbstractController
 {
     #[Route('/register', name: 'app_register')]
-    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager): Response
+    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager, MailerInterface $mailer, #[Autowire('%admin_email%')] string $adminEmail): Response
     {
         $user = new User();
         $form = $this->createForm(RegistrationFormType::class, $user);
@@ -32,6 +35,16 @@ class RegistrationController extends AbstractController
             $entityManager->persist($user);
             $entityManager->flush();
             // do anything else you need here, like send an email
+            $email = (new TemplatedEmail())
+                ->from($adminEmail)
+                ->to($user->getEmail())
+                ->subject('Your account have been created')
+                ->htmlTemplate('emails/register.html.twig')
+                ->context([
+                    'user' => $user
+                ]);
+
+            $mailer->send($email);    
 
             return $this->redirectToRoute('app_home');
         }
